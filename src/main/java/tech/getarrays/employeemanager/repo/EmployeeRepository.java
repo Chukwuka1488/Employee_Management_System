56278dc1b0f7939b43e302aa63ef3d9165c47189
Chuks Akibot <juniorakibor@yahoo.com>
2022-01-06 02:42:02 +0100

    postman crud functional

---
+++ src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package tech.getarrays.employeemanager.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import tech.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+
+
+
+    void deleteEmployeeById(String id);
+
+
+    Optional<Employee> findEmployeeById(String id);
+
+    Employee findEmployeeByVorname(String vorname);
+}
