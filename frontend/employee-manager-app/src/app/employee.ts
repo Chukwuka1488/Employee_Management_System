ca339f6ecd6a629c60f6b923d763dd5495125bbf
Chuks Akibot <juniorakibor@yahoo.com>
2022-01-06 14:32:17 +0100

    CORS implementation to enable FE & BE connectivity

--- frontend/employee-manager-app/src/app/employee.ts
+++ frontend/employee-manager-app/src/app/employee.ts
@@ -1,24 +1,24 @@
 export interface Employee {
   id: string
-  SAP_Personalnummer: number
-  Spalte1: string
-  Vorname: string
-  Nachname: string
-  RI: string
-  Verfugbarkeit: string
-  Berufserfahrung: string
-  ANU: string
-  Mobilitat: string
-  Kompetenzen: string
-  Tools: string
-  Sprachen: string
-  RT: string
-  Aktionen: string
-  Projektwunsch: string
-  Schwerpunkt: string
-  Division: string
-  Einheit: string
-  Position_RI: string
-  Manager1: string
-  Manager2: string
+  sap_Personalnummer: number
+  spalte1: string
+  vorname: string
+  nachname: string
+  ri: string
+  verfugbarkeit: string
+  berufserfahrung: string
+  anu: string
+  mobilitat: string
+  kompetenzen: string
+  tools: string
+  sprachen: string
+  rt: string
+  aktionen: string
+  projektwunsch: string
+  schwerpunkt: string
+  division: string
+  einheit: string
+  position_RI: string
+  manager1: string
+  manager2: string
 }
